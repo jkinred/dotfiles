9726ff7d39252a1d9d761a0c067cd95f8846f3f6
Jonathan Kinred <jkinred@linuxfoundation.org>
2024-02-26 19:45:31 +1100

    feat: Upgrade to new P27h-30's

--- dot_config/waybar/scripts/executable_switch-output.sh
+++ dot_config/waybar/scripts/executable_switch-output.sh
@@ -2,17 +2,18 @@
 
 waybar_dir="${HOME}/.config/waybar"
 config_file="${waybar_dir}/config"
+primary_monitor=$(grep Lenovo "${HOME}/.config/kanshi/config" | head -1 | awk -F\" '{print $2}')
 
 to=$1
 
 if [ "$to" == "multi" ]; then
-  waybar_to="Lenovo Group Limited P27h-20 V9075P1R"
+  waybar_to=$primary_monitor
 else
   waybar_to="eDP-1"
 fi
 
 pushd "$waybar_dir" || exit
-sed -i "s/^  \"output\":.*,$/  \"output\": \"$waybar_to\",/" $config_file
+sed -i "s/^  \"output\":.*,$/  \"output\": \"$waybar_to\",/" "$config_file"
 popd || exit
 
 echo "${to} activated successfully!" >> /tmp/foo
